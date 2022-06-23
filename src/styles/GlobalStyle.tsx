1f22ce289eff17a9079d4d4ad0b6c1a9a42e9ccf
hyebin <sksp4334@naver.com>
2022-06-23 20:12:23 +0900

    feat: GlobalStyle 추가

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        margin:0;
+        padding:0;
+        box-sizing:border-box;
+    }
+    li{
+        list-style:none;
+    }
+    a{
+        color:inherit;
+        text-decoration:none;
+        cursor:pointer;
+    }
+    input{
+        outline:none;
+    }
+`;
+
+export default GlobalStyle;
