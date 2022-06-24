f6768bbb2aaca19e6f1fb177e3f25bf509682664
hyebin <sksp4334@naver.com>
2022-06-25 01:42:22 +0900

    refactor: useCountUp 에러제거 및 변수 이름 변경

--- src/hooks/useCountUp.tsx
+++ src/hooks/useCountUp.tsx
@@ -1,57 +1,62 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from 'react'
 
-export default function useCountUp(
-  second: number,
-  end: number,
+interface UseCountUpType {
+  second: number
+  end: number
   slowPoint: number
-) {
-  const [count, setCount] = useState(0);
-  const requestRef = useRef<number>(0);
-  const slowCountRef = useRef<number>(0);
+}
 
-  const SLOWSTARTTIME = 50;
+export default function useCountUp({ second, end, slowPoint }: UseCountUpType) {
+  const [count, setCount] = useState(0)
+  const requestRef = useRef<number>(0)
+  const slowCountRef = useRef<number>(0)
 
-  const frame = second * 60;
-  const slowEndTime = frame - SLOWSTARTTIME;
-  const frameSlowOneCount = Math.floor(slowEndTime / (end - slowPoint));
+  const SLOWSTARTTIME = 50
+
+  const frame = second * 60
+  const slowEndTime = frame - SLOWSTARTTIME
+  const frameSlowOneCount = Math.floor(slowEndTime / (end - slowPoint))
 
   const animate = () => {
-    if (requestRef.current! > frame) {
-      setCount(end);
-      return;
+    if (requestRef.current > frame) {
+      return false
     }
 
-    if (requestRef.current! <= SLOWSTARTTIME) {
-      setCount((prevCount) => prevCount + end / SLOWSTARTTIME);
+    if (requestRef.current <= SLOWSTARTTIME) {
+      setCount((prevCount) => prevCount + slowPoint / SLOWSTARTTIME)
     }
 
-    if (requestRef.current! === SLOWSTARTTIME) setCount(slowPoint);
+    if (requestRef.current === SLOWSTARTTIME) {
+      setCount(slowPoint)
+    }
 
-    if (requestRef.current! > SLOWSTARTTIME && frameSlowOneCount !== 0) {
-      slowCountRef.current += 1;
+    if (requestRef.current > SLOWSTARTTIME && frameSlowOneCount !== 0) {
+      slowCountRef.current += 1
       if (slowCountRef.current >= frameSlowOneCount) {
         setCount((prev) => {
-          if (prev === end) return prev;
-          return prev + 1;
-        });
-        slowCountRef.current = 0;
+          if (prev === end) {
+            return prev
+          }
+          return prev + 1
+        })
+        slowCountRef.current = 0
       }
     }
 
     if (frameSlowOneCount === 0) {
-      setCount((prevCount) => prevCount + (end - slowPoint) / 80);
+      setCount((prevCount) => prevCount + (end - slowPoint) / slowEndTime)
     }
 
-    requestRef.current! += 1;
-    requestAnimationFrame(animate);
-  };
+    requestRef.current += 1
+    requestAnimationFrame(animate)
+  }
 
   useEffect(() => {
-    requestAnimationFrame(animate);
+    requestAnimationFrame(animate)
     return () => {
-      cancelAnimationFrame(requestRef.current!);
-    };
-  }, []);
+      cancelAnimationFrame(requestRef.current!)
+    }
+  }, [])
 
-  return Math.floor(count);
+  return Math.floor(count)
 }
